dc6dc35834f83503d6f3facd0e423f5f9cd4d7c0
PrimeFord <bashfordbash@gmail.com>
2023-01-05 23:25:38 +0100

    Calculator

---
+++ main.js
@@ -0,0 +1,114 @@
+//Query selector
+const number = document.querySelectorAll(".number");
+const result = document.querySelector(".result span");
+const sign = document.querySelector(".sign");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const pos_neg = document.querySelector(".pos_neg");
+const percent = document.querySelector(".percent");
+
+//
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let signs = "";
+let resultValue = 0;
+
+//
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", (e) => {
+    let atr = e.target.getAttribute("value");
+    if (isFirstValue === false) {
+      getFirstValue(atr);
+    }
+    if (isSecondValue == false) {
+      getFirstValue(atr);
+    }
+  });
+}
+
+//
+function getFirstValue(el) {
+  result.innerHTML = "";
+  firstValue += el;
+  result.innerHTML = firstValue;
+  firstValue = +firstValue;
+}
+
+function getSecondValue(el) {
+  if (firstValue != "" && sign != "") {
+    secondValue += el;
+    result.innerHTML = secondValue;
+    secondValue = +secondValue;
+  }
+}
+
+function getSign() {
+  for (let i = 0; i < sign.length; i++) {
+    sign[i].addEventListener("click", (e) => {
+      signs = e.target.getAttribute("value");
+      isFirstValue = true;
+    });
+  }
+}
+getSign();
+
+equal.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (signs === "+") {
+    resultValue = firstValue + secondValue;
+  } else if (signs === "-") {
+    resultValue = firstValue - secondValue;
+  } else if (signs === "x") {
+    resultValue = firstValue * secondValue;
+  } else if (signs === "/") {
+    resultValue = firstValue / secondValue;
+  }
+  result.innerHTML = resultValue;
+  firstValue = resultValue;
+  secondValue = "";
+});
+
+function checkResultLength() {
+  resultValue = JSON.stringify(resultValue);
+  if (resultValue.length >= 8) {
+    resultValue = JSON.parse(resultValue);
+    result.innerHTML = resultValue.toFixed(5);
+  }
+}
+
+pos_neg.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (firstValue != "") {
+    resultValue = -firstValue;
+    firstValue = resultValue;
+  }
+  if (firstValue != "" && secondValue != "" && sign != "") {
+    resultValue = -resultValue;
+  }
+  result.innerHTML = resultValue;
+});
+
+percent.addEventListener("click", () => {
+  result.innerHTML = "";
+  if (firstValue != "") {
+    resultValue = firstValue / 100;
+    firstValue = resultValue;
+  }
+  if (firstValue != "" && secondValue != "" && sign != "") {
+    resultValue = resultValue / 100;
+  }
+  result.innerHTML = resultValue;
+});
+
+clear.addEventListener("click", () => {
+  result.innerHTML = 0;
+
+  firstValue = "";
+  isFirstValue = false;
+  secondValue = "";
+  isSecondValue = false;
+  sign = "";
+  resultValue = 0;
+});
